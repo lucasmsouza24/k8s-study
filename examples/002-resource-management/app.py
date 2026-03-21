d2fdb7587f96aae0d32a7d587f6a89059188c161
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2026-03-21 14:37:04 -0300

    feat: :sparkles: adding endpoints do break cpu and memory resources

--- examples/002-resource-management/app.py
+++ examples/002-resource-management/app.py
@@ -5,3 +5,25 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'hello': 'world'}
+
+# 🔥 CPU-bound
+@app.get('/cpu-bound')
+def consume_cpu():
+    x = 0
+    for i in range(10**8):
+        x += i
+    return {"result": x}
+
+# 💥 Memory leak
+memory_holder = []
+
+@app.get('/memory')
+def consume_memory():
+    # adding ~10MB for each request
+    chunk = "x" * (10 * 1024 * 1024)
+    memory_holder.append(chunk)
+
+    return {
+        "chunks": len(memory_holder),
+        "approx_mb": len(memory_holder) * 10
+    }
